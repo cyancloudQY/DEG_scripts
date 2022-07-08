b9a38cb154a31c6ecb9354b8625d364bf0d439cd
cyancloudQY <99325498+cyancloudQY@users.noreply.github.com>
2022-07-08 15:21:53 +0800

    Add files via upload

--- GO_KEGG.R
+++ GO_KEGG.R
@@ -16,7 +16,63 @@ work_dir <- opt$work
 gene_list <- read.table(input_file, header = FALSE)
 setwd(work_dir)
 
-print(gene_list)
+gene_list <- gene_list$V1
+
+
+
+######## GO ###########
+
+ego_all <- enrichGO(gene = gene_list, 
+                    OrgDb = 'org.Hs.eg.db', 
+                    ont = "ALL", 
+                    pAdjustMethod = "BH", 
+                    pvalueCutoff = 1,
+                    qvalueCutoff = 1,
+                    keyType = 'ENSEMBL',
+                    readable = TRUE)
+
+
+pdf('./go_barplot.pdf')
+barplot(ego_all,showCategory=10)
+dev.off()
+
+
+pdf('./go_dotplot.pdf')
+dotplot(ego_all,showCategory=10)
+dev.off()
+
+
+results_GO <- ego_all@result
+write.table(results_GO, 'results_GO.txt',quote = FALSE)
+
+
+
+
+####### KEGG ##########
+entrez_list <- bitr(gene_list, fromType = 'ENSEMBL',toType = 'ENTREZID', OrgDb="org.Hs.eg.db")
+
+
+
+
+kegg_all <- enrichKEGG(gene = entrez_list$ENTREZID,
+                       organism = "hsa",
+                       pvalueCutoff = 1,
+                       qvalueCutoff = 1)
+					   
+pdf('./kegg_barplot.pdf')
+barplot(kegg_all,showCategory=10)
+dev.off()
+
+
+pdf('./kegg_dotplot.pdf')
+dotplot(kegg_all,showCategory=10)
+dev.off()					 
+
+results_KEGG <- kegg_all@result
+write.table(results_KEGG, 'results_KEGG.txt',quote = FALSE)
+
+
+
 
 
 
